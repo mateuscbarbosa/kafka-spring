94ea3cf42ae693690acf8a83bf7a9e85c8aa9f86
Mateus Barbosa <mateuscbarbosa@gmail.com>
2024-01-09 11:28:35 -0300

    feature: configurations

--- pix-consumidor/src/main/java/com/alura/pix/config/ConsumerKafkaConfig.java
+++ pix-consumidor/src/main/java/com/alura/pix/config/ConsumerKafkaConfig.java
@@ -58,6 +58,16 @@ public class ConsumerKafkaConfig {
         props.put(
                 JsonDeserializer.TRUSTED_PACKAGES,
                 "*");
+        /* ignoradas na aula seguinte
+        props.put(
+        		ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10);
+        props.put(
+        		ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(
+        		ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG, false);
+        props.put(
+        		ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        */
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
--- pix-consumidor/src/main/java/com/alura/pix/consumidor/PixValidator.java
+++ pix-consumidor/src/main/java/com/alura/pix/consumidor/PixValidator.java
@@ -2,6 +2,7 @@ package com.alura.pix.consumidor;
 
 import com.alura.pix.dto.PixDTO;
 import com.alura.pix.dto.PixStatus;
+import com.alura.pix.exception.KeyNotFoundException;
 import com.alura.pix.model.Key;
 import com.alura.pix.model.Pix;
 import com.alura.pix.repository.KeyRepository;
@@ -11,6 +12,9 @@ import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,9 +26,17 @@ public class PixValidator {
     @Autowired
     private PixRepository pixRepository;
 
-    @KafkaListener(topics = "pix-topic", groupId = "grupo")
+    @KafkaListener(topics = "pix-topic-partitions", groupId = "grupo")
+    @RetryableTopic(
+    		backoff = @Backoff(value = 3000L),
+    		attempts = "5",
+    		autoCreateTopics = "true",
+    		include = KeyNotFoundException.class
+    )
     public void processaPix(PixDTO pixDTO) {
-        System.out.println("Pix  recebido: " + pixDTO.getIdentifier());
+    //public void processaPix(PixDTO pixDTO, Acknowledgment acknowledgment) {
+        //acknowledgment.acknowledge(); define quando o tópico sabe que foi consumida uma mensagem
+    	System.out.println("Pix  recebido: " + pixDTO.getIdentifier());
 
         Pix pix = pixRepository.findByIdentifier(pixDTO.getIdentifier());
 
@@ -33,6 +45,7 @@ public class PixValidator {
 
         if (Objects.isNull(origem) || Objects.isNull(destino)) {
             pix.setStatus(PixStatus.ERRO);
+            throw new KeyNotFoundException();
         } else {
             pix.setStatus(PixStatus.PROCESSADO);
         }
